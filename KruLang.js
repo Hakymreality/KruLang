0b2f55be845ee751c7d1931aff46a5b9b1305d88
hakym_unltd <clintonadeleke@gmail.com>
2020-10-26 13:36:43 +0100

    Completed Version 0.2

--- KruLang.js
+++ KruLang.js
@@ -126,11 +126,78 @@ specialForms.do = (args, scope) => {
     return value;
 };
 
-specialForms.define = (args,scope) => {
-    if (args.length != 2 || args[0].type != "word" ) {
+specialForms.define = (args, scope) => {
+    if (args.length != 2 || args[0].type != "word") {
         throw new SyntaxError("Incorrect use of define")
     }
-    let value = evaluate(args[1],scope);
+    let value = evaluate(args[1], scope);
     scope[args[0].name] = value;
     return value;
 };
+
+const globalScope = Object.create(null)
+globalScope.true = true;
+globalScope.false = false;
+
+let prog = parse(`if(true,false,true)`);
+console.log(evaluate(prog, globalScope))
+
+for (let op of ["+", "-", "*", "/", "==", "<", ">"]) {
+    globalScope[op] = Function("a,b", `return a ${op} b;`)
+}
+
+globalScope.print = value => {
+    console.log(value);
+    return value;
+};
+
+function run(program) {
+    return evaluate(parse(program), Object.create(globalScope))
+}
+
+run(`
+do(define(total, 0),
+    define(count, 1),
+    while(<(count, 11),
+        do(define(total, +(total, count)),
+            define(count, +(count, 1)))),
+    print(total))
+`);
+//Ln 162 adds the total to the count for each
+//iteration and redefines total
+
+specialForms.fun = (args, scope) => {
+    if (!args.length) {
+        throw new SyntaxError("Functions need a body");
+    }
+    let body = args[args.length - 1];
+    let params = args.slice(0, args.length - 1).map(expr => {
+        if (expr.type != "word") {
+            throw new SyntaxError("Parameter names must be words");
+        }
+        return expr.name;
+    });
+    211
+    return function () {
+        if (arguments.length != params.length) {
+            throw new TypeError("Wrong number of arguments");
+        }
+        let localScope = Object.create(scope);
+        for (let i = 0; i < arguments.length; i++) {
+            localScope[params[i]] = arguments[i];
+        }
+        return evaluate(body, localScope);
+    };
+};
+run(`
+    do(define(plusOne, fun(a, +(a, 1))),
+        print(plusOne(10)))
+`);
+
+run(`
+do(define(pow, fun(base, exp,
+    if(==(exp, 0),
+        1,
+        *(base, pow(base, -(exp, 1)))))),
+    print(pow(2, 10)))
+`);
